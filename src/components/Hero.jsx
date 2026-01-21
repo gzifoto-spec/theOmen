7abc753833ce519c40f2246b35434ae416e6ad83
Mariaregue-spec <mariareguecurso@gmail.com>
2026-01-21 10:35:49 +0100

    feat:hero.jsx and hero.png

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <section className="relative h-screen bg-[url('/hero.jpg')] bg-cover bg-center">
+            <div className="absolute inset-0 bg-black/60" />
+        </section>
+    );
+}
