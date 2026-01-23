05c68fed87fc000b61d04fc30bfd349f243b1ea6
Mariaregue-spec <mariareguecurso@gmail.com>
2026-01-23 14:07:54 +0100

    feat: apply navbar styles and remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import Peliculas from "./pages/SeccionDePeliculas"
-import Formulario from "./pages/FormularioParaAnadirPeliculas"
-import Ubicacion from "./pages/Ubicacion"
-import Nosotros from "./pages/SobreNosotros"
-import Contacto from "./pages/Contacto"
+import Peliculas from "./pages/MoviesSection"
+import Formulario from "./pages/FormMovies"
+import Nosotros from "./pages/AboutUs"
+import Contacto from "./pages/Contact"
 import Footer from "./components/Footer"
 
 
@@ -17,11 +16,10 @@ function App () {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/peliculas" element={<Peliculas />} />
-          <Route path="/formulario" element={<Formulario />} />
-          <Route path="/nosotros" element={<Nosotros />} />
-          <Route path="/contacto" element={<Contacto />} />    
-          <Route path="/ubicacion" element={<Ubicacion />} />
+          <Route path="/movies" element={<Peliculas />} />
+          <Route path="/form" element={<Formulario />} />
+          <Route path="/aboutus" element={<Nosotros />} />
+          <Route path="/contact" element={<Contacto />} />    
         </Routes>
       </main>
       <Footer />
--- src/pages/Ubicacion.jsx
+++
@@ -1,9 +0,0 @@
-const Ubicacion = () => {
-  return (
-    <h1 className="text-3xl font-bold text-center mt-10 bg-gray-200 p-5">
-        Ubicación Page
-      </h1>
-  )
-}
-
-export default Ubicacion
