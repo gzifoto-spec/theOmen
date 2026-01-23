fa200271802864588eca3bb786efb779f4c761bf
gzifoto-spec <gzifoto@gmail.com>
2026-01-23 13:22:51 +0100

    fix: change SobreNosotros.jsx to AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 const Nosotros = () => {
   return (
     <h1 className="text-3xl font-bold text-center mt-10 bg-gray-200 p-5">
-        Sobre nosotros Page
+        Sobre nosotros Page cambio de prueba
       </h1>
   )
 }
