eab23e50e8b717e98eaa22322ed06b082733b23b
gzifoto-spec <gzifoto@gmail.com>
2026-01-25 17:08:44 +0100

    fix: deleting unexisting localizacion import and fixing App.jsx routes to english named ones

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Peliculas from "./pages/MoviesSection"
-import Formulario from "./pages/FormularioParaAnadirPeliculas"
+import Formulario from "./pages/FormMovies"
 import Nosotros from "./pages/AboutUs"
 import Contacto from "./pages/Contact"
 import Footer from "./components/Footer"
@@ -16,11 +16,11 @@ function App () {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/peliculas" element={<Peliculas />} />
-          <Route path="/formulario" element={<Formulario />} />
-          <Route path="/nosotros" element={<Nosotros />} />
-          <Route path="/contacto" element={<Contacto />} />    
-          <Route path="/ubicacion" element={<Ubicacion />} />
+          <Route path="/movies" element={<Peliculas />} />
+          <Route path="/form" element={<Formulario />} />
+          <Route path="/aboutus" element={<Nosotros />} />
+          <Route path="/contact" element={<Contacto />} />    
+        
         </Routes>
       </main>
       <Footer />
